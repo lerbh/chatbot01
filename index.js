90003a6618de009be171cf8db2705961ab7289f0
lerbh <lerbenghin2@gmail.com>
2018-11-27 21:13:45 +0800

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.get("/",function(req,res){
+	res.send('<h1>This is my web app');
+});
+
+
+	var listener = app.listen(process.env.PORT,process.env.IP,function(){
+		console.log("server has started");
+	    console.log('Listening on port ' + listener.address().port);
+});
